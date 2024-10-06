79da83d45f984538161ff307807034c5f828a687
Pito Salas <pitosalas@users.noreply.github.com>
2024-10-06 17:24:32 -0400

    remove files from git hist

--- inwallout/Jiahao Li/wall_follower/src/logger.py
+++
@@ -1,8 +0,0 @@
-class Logger:
-    def __init__(self):
-        self.log_count = 0
-    
-    def log(self, msg):
-        if self.log_count % 10 == 0:
-            print(msg)
-        self.log_count += 1
